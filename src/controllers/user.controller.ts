5bd7383c9fa13357e56de9d73eddf3e14255aaa3
alifdelgado <alifdelgado@gmail.com>
2023-08-01 01:36:29 -0600

    Create user method added to service

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,9 +1,20 @@
 import { Request, Response } from "express";
 import { CreateUserInput } from "../schemas/user.schema";
+import { createUser } from "../services/user.service";
 
-export const createUserHandler = (
+export const createUserHandler = async (
   req: Request<{}, {}, CreateUserInput>,
   res: Response
 ) => {
   const body = req.body;
+
+  try {
+    const user = await createUser(body);
+    return res.status(201).send("User created successfully");
+  } catch (e) {
+    if (e.code === 11000) {
+      return res.status(409).send("User already exists");
+    }
+    return res.status(500).send("Error creating user");
+  }
 };
--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,1 +1,5 @@
-export const createUser = () => {};
+import UserModel, { User } from "../models/user.model";
+
+export const createUser = (input: Partial<User>) => {
+  return UserModel.create(input);
+};
